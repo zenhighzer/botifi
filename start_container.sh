d92971c07f9bf85fcee82defd527c10a86c1cd08
zenhighzer <65350020+zenhighzer@users.noreply.github.com>
2025-03-14 11:13:25 +0100

    Update start_container.sh

--- start_container.sh
+++ start_container.sh
@@ -4,14 +4,14 @@ read -s -p "Discord BOT Token: " DISCORD_BOT_TOKEN
 echo
 read -s -p "UNIFI API Key: " UNIFI_API_KEY
 echo
-read -s -p "UNIFI API URL / WLAN, i.e: https://192.168.1.1/proxy/network/api/s/default/rest/wlanconf/76sdf04sdf4234fsdsf233ed: " UNIFI_API_URL
+read -s -p "UNIFI API URL, like https://unifi.local/proxy/network/api/s/default/rest/wlanconf/234lkjl234aasda: " UNIFI_API_URL
 echo
 
 export DISCORD_BOT_TOKEN
 export UNIFI_API_KEY
 export UNIFI_API_URL
 
-docker run -e DISCORD_BOT_TOKEN=$DISCORD_BOT_TOKEN -e UNIFI_API_KEY=$UNIFI_API_KEY -e UNIFI_API_URL=$UNIFI_API_URL --restart always --name botifi -dit botifi
+docker run --add-host unifi.local:192.168.100.100 -e DISCORD_BOT_TOKEN=$DISCORD_BOT_TOKEN -e UNIFI_API_KEY=$UNIFI_API_KEY -e UNIFI_API_URL=$UNIFI_API_URL --restart always --name botifi -dit botifi
 
 export DISCORD_BOT_TOKEN=null
 export UNIFI_API_KEY=null
